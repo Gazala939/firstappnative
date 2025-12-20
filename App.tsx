bb292ccdc3497b7044c44b30099f9e45f3aeba00
Gazala939 <Gazalajamadar786@gmail.com>
2025-12-20 20:07:11 +0530

    Add the styleSheet

--- App.tsx
+++ App.tsx
@@ -1,14 +1,44 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ color: 'red', fontSize: 34 }}>
-        Hello World !!!
-      </Text>
+    <View style = {styles.container}>
+      <Text style = {styles.title}> 👋🏻 Welcome</Text>
+      <Text style = {styles.name}> Name: Gazala Jamadar</Text>
+      <Text style = {styles.course}>Course: React Native </Text>
+
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+   
+    container:{
+        flex:1,
+        justifyContent : 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+    },
+
+    title:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        color:'blue'
+    },
+
+    name:{
+        fontSize: 20,
+        color:'Black',
+        marginTop:10,
+    },
+
+    course:{
+        color:'green',
+        marginTop:10,
+        fontSize:18,
+    },
+
+
+})
 export default App;
 
