0a90d0134694bd9cc4a0a60343f3dbb82ac3b999
Gazala939 <Gazalajamadar786@gmail.com>
2025-12-20 19:28:51 +0530

    Update App component UI

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
-
-
-import {  Text,  View } from 'react-native';
+import { Text, View } from 'react-native';
 
 function App() {
-    return (
-        <View>
-            <Text> hi i am gazala</Text>
-        </View>
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: 'red', fontSize: 34 }}>
+        Hello World !!!
+      </Text>
+    </View>
   );
 }
 
 export default App;
+
