31b0497cc8f511679465e2104832c5ea6b451ee2
Gazala939 <Gazalajamadar786@gmail.com>
2025-12-29 21:00:59 +0530

    Light and Dark Theme

--- App.tsx
+++ App.tsx
@@ -1,62 +1,32 @@
 import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  Button,
-} from 'react-native';
+import { View, Text, Switch } from 'react-native';
 
 export default function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  // ✅ email validation
-  const isEmailValid =
-    email.includes('@') && email.length >= 10;
-
-  const submit = () => {
-    alert(`Email: ${email}\nPassword: ${password}`);
-  };
+  const [dark, setDark] = useState(false);
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        onChangeText={setEmail}
-      />
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: dark ? '#000' : '#fff',
+      }}
+    >
+      <Text
+        style={{
+          marginBottom: 10,
+          color: dark ? '#fff' : '#000',
+          fontSize: 18,
+        }}
+      >
+        Dark Mode
+      </Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        onChangeText={setPassword}
+      <Switch
+        value={dark}
+        onValueChange={setDark}
       />
-
-      {/* ✅ Button instead of TouchableOpacity */}
-      <Button
-        title="Submit"
-        onPress={submit}
-        disabled={!isEmailValid}
-      />
-
-     
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 20,
-  },
-  input: {
-    borderWidth: 1,
-    marginBottom: 10,
-    padding: 10,
-    borderRadius: 6,
-  },
-  
-});
