c943f827a8a52f4b8fad6d6951690407ee60e10b
Gazala939 <Gazalajamadar786@gmail.com>
2026-01-12 16:11:19 +0530

    Names Added through list

--- App.tsx
+++ App.tsx
@@ -1,34 +1,28 @@
-import React, { useState } from 'react';
-import {
-  ScrollView,
-  RefreshControl,
-  Text,
-  View,
-} from 'react-native';
+import React from 'react'
+import {View,Text,StyleSheet} from 'react-native';
 
-export default function App() {
-  const [refreshing, setRefreshing] = useState(false);
+function App(){
+    const names = ['Farha','Sara', 'Zara', 'Gazala'];
+    return(
+        <View style = {styles.container}>
+            {names.map((obj , index)=>{
+                return(
+                    <Text style = {{fontSize:20}} key = {index}>
+                        {obj}
+                    </Text>
+                )
+            })}
+        </View>
 
-  const onRefresh = async () => {
-    setRefreshing(true);
+    )
 
-    // simulate API call
-    await new Promise(resolve => setTimeout(resolve, 2000));
-
-    setRefreshing(false);
-  };
-
-  return (
-    <ScrollView
-      contentContainerStyle={{ padding: 20 }}
-      refreshControl={
-        <RefreshControl
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-        />
-      }
-    >
-      <Text>Pull down to refresh</Text>
-    </ScrollView>
-  );
 }
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
+
+export default App
